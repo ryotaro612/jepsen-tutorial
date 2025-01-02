0bccbc6b532318d946a8301b5e9c7933662ae39a
Ryotaro <ryotaro612.ooo@gmail.com>
2025-01-03 00:17:42 +0900

    chore: serve the test results on 8080

--- jepsen.etcd/src/jepsen/etcd.clj
+++ jepsen.etcd/src/jepsen/etcd.clj
@@ -1,5 +1,6 @@
 (ns jepsen.etcd
   (:require [jepsen.cli :as cli]
+            [jepsen.db :as db]
             [jepsen.tests :as tests]))
 
 (defn etcd-test
@@ -10,16 +11,18 @@
   (println (merge tests/noop-test
          {:pure-generators true}
          opts), "####")
+  (db "")
   (merge tests/noop-test
          {:pure-generators true}
+         ; private-key-path must be an absolute path
          {:concurrency 5, :leave-db-running? false, :logging-json? false, :ssh {:dummy? false, :username "root", :strict-host-key-checking false, :port 8022 :private-key-path "~/.ssh/id_rsa"},  :nodes ["127.0.0.1"], :test-count 1, :time-limit 60}
-         )
-  
-  )
+         ))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
   browsing results."
   [& args]
-  (cli/run! (cli/single-test-cmd {:test-fn etcd-test})
+  (cli/run! (merge (cli/single-test-cmd {:test-fn etcd-test})
+                   (cli/serve-cmd))
             args))
+
