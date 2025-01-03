a6b09e22a973cf62ef5eeeedf6084d17959d05a5
Ryotaro <ryotaro612.ooo@gmail.com>
2025-01-03 14:34:30 +0900

    fix: fix the path to a private key

--- jepsen.etcd/src/jepsen/etcd.clj
+++ jepsen.etcd/src/jepsen/etcd.clj
@@ -1,8 +1,25 @@
 (ns jepsen.etcd
-  (:require [jepsen.cli :as cli]
-            [jepsen.db :as db]
-            [jepsen.tests :as tests]))
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [clojure.java.io :as io]
+            [jepsen [cli :as cli]
+             [control :as c]
+             [db :as db]
+             [tests :as tests]]
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as debian]))
 
+(defn db
+  "Etcd DB for a particular version."
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing etcd" version))
+
+    (teardown! [_ test node]
+      (info node "tearing down etcd"))))
+
+; setupで前のデータをリセットしたほうがいい
 (defn etcd-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
@@ -13,9 +30,15 @@
          opts), "####")
   (db "")
   (merge tests/noop-test
-         {:pure-generators true}
+         {
+          :name "etcd"
+          :os   debian/os
+          :db   (db "v3.1.5")
+          :pure-generators true}         
          ; private-key-path must be an absolute path
-         {:concurrency 5, :leave-db-running? false, :logging-json? false, :ssh {:dummy? false, :username "root", :strict-host-key-checking false, :port 8022 :private-key-path "~/.ssh/id_rsa"},  :nodes ["127.0.0.1"], :test-count 1, :time-limit 60}
+         {:concurrency 5, :leave-db-running? false, :logging-json? false, :ssh {:dummy? false, :username "root", :strict-host-key-checking false,:port 8022 :private-key-path (.getAbsolutePath (io/file ".." "id_rsa"))}
+          :nodes ["127.0.0.1"], :test-count 1, :time-limit 60}
+
          ))
 
 (defn -main
